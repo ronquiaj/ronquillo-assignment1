7dd6fb2121a30e7e5e86c7018ea329dda5dfc530
Adrian Ronquillo <ronquiaj@plu.edu>
2021-03-06 08:27:47 -0900

    Added another variable to Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Hello {
     public static void main(String[] args) {
         int hi = 2;
+        int b = 0;
         System.out.println("Hello there");
         ArrayList<Integer> aList = new ArrayList<Integer>();
     }

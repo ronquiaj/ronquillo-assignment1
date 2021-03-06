89a310430d8582a8834fce0041ae4397e8721603
Adrian Ronquillo <ronquiaj@plu.edu>
2021-03-06 14:55:47 -0900

    deleted everything in Hello.java lol

--- Hello.java
+++ Hello.java
@@ -2,10 +2,6 @@ import java.util.*;
 
 public class Hello {
     public static void main(String[] args) {
-        int hi = 2;
-        int b = 0;
-        System.out.println("Hello there");
-        ArrayList<Integer> aList = new ArrayList<Integer>();
-        int g = 0;
+       
     }
 }

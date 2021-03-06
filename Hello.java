ef79a5692e79d14304321d77ee2a1762b75c18c2
Adrian Ronquillo <ronquiaj@plu.edu>
2021-03-06 14:52:54 -0900

    added a variable to hello.java withthe string This is the final part

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         int hi = 2;
         int b = 0;
+        String partFive = "This is the final part";
         System.out.println("Hello there");
         ArrayList<Integer> aList = new ArrayList<Integer>();
     }

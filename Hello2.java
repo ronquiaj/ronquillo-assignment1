a351389af7f290c33b38a7aa87ba4f43ed884a61
Adrian Ronquillo <ronquiaj@plu.edu>
2021-03-06 08:11:47 -0900

    added another variable and print statement to Hello2.java

--- Hello2.java
+++ Hello2.java
@@ -1,6 +1,7 @@
 public class Hello2 {
     public static void main(String[] args) {
         int x = 0;
-        System.out.println("The value of x is: " + x);
+        int y = 1;
+        System.out.println("The value of x is: " + x + " the value of y is: " + y);
     }
 }
